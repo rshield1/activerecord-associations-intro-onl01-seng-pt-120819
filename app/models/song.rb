a78ee4e018b0680d7060349bf2b1df03bf7b28a1
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-08 07:31:17 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,21 @@
 class Song < ActiveRecord::Base
-  
   belongs_to :artist
   belongs_to :genre
-  
+
+  def get_genre_name
+    self.genre.name
+  end
+
+  def drake_made_this
+    if Artist.all.any?{|i| i.name == "Drake"}
+      self.artist = drake = Artist.find_by(name: "Drake")
+      self.save
+      drake.songs.push(self)
+    else
+      drake = Artist.new(name: "Drake")
+      self.artist = drake
+      drake.save
+      drake.songs.push(self)
+    end
+  end
 end
--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,11 +1,7 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-  
   def change
     create_table :songs do |t|
-      t.string :name
-      t.integer :artist_id
-      t.integer :genre_id
+      t.string :name 
     end
   end
-  
 end
